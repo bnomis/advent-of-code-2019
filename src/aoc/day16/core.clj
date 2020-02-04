71ce42c97dad7573455db9add53dfed79553b629
Simon Blanchard <bnomis@gmail.com>
2020-02-04 20:07:19 +0800

    day 16 part 2

--- src/aoc/day16/core.clj
+++ src/aoc/day16/core.clj
@@ -27,6 +27,10 @@
   (into [] (map #(Integer/parseInt %1) (str/split string #""))))
 
 
+(defn str-to-int [string]
+  (Integer/parseInt string))
+
+
 (defn last-digit [inum]
   (last (string-to-ints (str (Math/abs inum)))))
 
@@ -55,11 +59,31 @@
         out
         (recur (inc position) (conj out (row-to-last-digit row position)))))))
 
+; assume the offset is over half way
+; we can ignore everything before it
+; it just becomes a sum
+(defn row-to-row-offset [row offset]
+  (let [length (count row)]
+    (loop [index 0
+           out []
+           sum (reduce + (drop offset row))]
+      (if (= index length)
+        out
+        (if (< index offset)
+          (recur (inc index) (conj out 0) sum)
+          (recur (inc index) (conj out (mod sum 10)) (- sum (get row index))))))))
+
 
 (defn row-phases [row phases]
   (if (= 0 phases)
     row
     (row-phases (row-to-row row) (dec phases))))
+
+
+(defn row-phases-with-offset [row phases offset]
+  (if (= 0 phases)
+    row
+    (row-phases-with-offset (row-to-row-offset row offset) (dec phases) offset)))
 
 
 (defn string-phases [string phases]
@@ -70,12 +94,20 @@
   (take 8 row))
 
 
+(defn first-x [row x]
+  (take x row))
+
+
 (defn ints-to-string [ints]
   (str/join (map str ints)))
 
 
 (defn row-first-eight [row]
   (ints-to-string (first-eight row)))
+
+
+(defn row-first-x-to-string [row x]
+  (ints-to-string (first-x row x)))
 
 
 (defn file-to-string [filename]
@@ -90,6 +122,27 @@
   (row-first-eight (file-phases filename phases)))
 
 
+(defn repeat-vector [vector repeats]
+  (reduce (fn [c v] (into c vector)) [] (range repeats)))
+
+
+(defn row-x-at-offset [row x offset]
+  (ints-to-string (reduce (fn [c i] (conj c (get row (+ offset i)))) [] (range x))))
+
+
+(defn string-repeat-offset [string phases repeats offset]
+  (let [ints (string-to-ints string)
+        vector (repeat-vector ints repeats)
+        s (row-first-x-to-string ints offset)
+        i (str-to-int s)
+        row (row-phases-with-offset vector phases i)]
+    (row-x-at-offset row 8 i)))
+
+
+(defn file-repeat-offset [filename phases repeats offset]
+  (string-repeat-offset (file-to-string filename) phases repeats offset))
+
+
 (defn run []
   (println "Day 16, part 1:" (file-phases-first-eight "src/aoc/day16/input.txt" 100))
-  (println "Day 16, part 2:"))
+  (println "Day 16, part 2:" (file-repeat-offset "src/aoc/day16/input.txt" 100 10000 7)))
